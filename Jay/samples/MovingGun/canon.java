d4d36e3e46e8dc4bdfab67d0b0fd8d93471c9342
Jay <janhudesai@gmail.com>
2017-09-28 17:02:43 -0700

    issue#10 Developed sample code for moving gun object.

---
+++ Jay/samples/MovingGun/canon.java
@@ -0,0 +1,42 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class canon extends Actor
+{
+
+    /**
+     * Act - do whatever the canon wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("left")) {
+            setRotation(getRotation() - 5);
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            setRotation(getRotation() + 5);
+        }
+        if (Greenfoot.isKeyDown("up")) {
+            setLocation(getX(), getY() - 5);
+        }
+        if (Greenfoot.isKeyDown("down")) {
+            setLocation(getX(), getY() + 5);
+        }
+        if ("space".equals(Greenfoot.getKey())) {
+            fire();
+        }
+    }
+
+    /**
+     * 
+     */
+    private void fire()
+    {
+        blob blob1 =  new  blob();
+        getWorld().addObject(blob1, getX(), getY());
+        blob1.setRotation(getRotation());
+    }
+}
