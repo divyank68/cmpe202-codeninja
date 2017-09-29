04532606d271867fa1d08bab4e85e9a8f8cd2fd6
Jay <janhudesai@gmail.com>
2017-09-28 17:14:34 -0700

    issue#11 Developed Character getting shot as a sample code for Code Ninja Project

---
+++ Jay/samples/CharacterGettingShot/MyWorld.java
@@ -0,0 +1,37 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(600, 400, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        canon canon =  new  canon();
+        Wall wall1 =  new  Wall();
+        Wall wall2 =  new  Wall();
+        Wall wall3 =  new  Wall();
+        Wall wall4 =  new  Wall();
+        addObject(canon, 100, 248);
+        addObject(wall1, 400, 248);
+        addObject(wall2, 200, 50);
+        addObject(wall3, 300, 300);
+        addObject(wall4, 500, 100);
+    }
+}
---
+++ Jay/samples/MovingGun/README.md
@@ -0,0 +1,5 @@
+PROJECT TITLE: Moving Gun Object
+PURPOSE OF PROJECT: Developed as a sample code for Code Ninja Project.
+VERSION or DATE: 09/28/2017
+HOW TO START THIS PROJECT: Run project.greenfoot file to start the sample.
+AUTHORS: Jay Desai
