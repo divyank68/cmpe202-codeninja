e8b917196e246390908b7df0f5c7223b68d2d2c2
Utsav Jain <utsavjain1408@gmail.com>
2017-10-23 23:56:34 -0700

    Adding code for moving bricks

--- Utsav Jain/EarthWorld/MovingBrick.java
+++ Utsav Jain/EarthWorld/MovingBrick.java
@@ -12,6 +12,7 @@ public class MovingBrick extends Actor
      * Act - do whatever the MovingBrick wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int counter= 0;
      public MovingBrick(){
     }
     public MovingBrick(int x, int y){
@@ -19,8 +20,29 @@ public class MovingBrick extends Actor
         image.scale(x,y);
         setImage(image);
     }
+    void move(){
+        int  x, y;
+        x= getX();
+        y= getY();
+        
+        if(counter>=0&&counter<20){
+            setLocation(x++,y);
+            counter--;
+        }
+        else{
+            setLocation(x--,y);
+            if(counter>=-20){
+            counter=20;
+            }
+        }
+    }
     public void act() 
     {
+        move();
+        int  x, y;
+        x= getX();
+        y= getY();
+         this.setLocation(x++,y);
         // Add your action code here.
     }    
 }
