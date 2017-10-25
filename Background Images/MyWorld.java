e6ce4142780ead5cc6fbb58de3ed19ee4cffc974
Shantanu <shantanugupta2828@gmail.com>
2017-10-24 23:43:41 -0700

    Infinite Scrolling Background

---
+++ Background Images/MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    private Background1 img0, img1;
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1, false);
+        
+        img0= new Background1();
+        addObject(img0, getWidth()/2, getHeight()/2 );
+        
+        img1 = new Background1();
+        addObject( img1, getWidth() + getWidth()/2, getHeight()/2 );
+    }
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("right")){
+            img0.scroll();
+            img1.scroll();
+        }
+        }
+    
+}
