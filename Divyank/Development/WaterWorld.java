fbe4503c92eedc24c6319402044015aae37be074
Divyank <divyank68@yahoo.co.in>
2017-10-25 23:05:56 -0700

    Issue#44 Adding ninja to the water world

--- Divyank/Development/WaterWorld.java
+++ Divyank/Development/WaterWorld.java
@@ -17,5 +17,7 @@ public class WaterWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Ninja n = new Ninja();
+        addObject(n, 100, 180);
     }
 }
