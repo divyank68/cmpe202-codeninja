27c8b5733d1d7495da42b8297f3e2503c8186295
Divyank <divyank68@yahoo.co.in>
2017-10-26 18:53:00 -0700

    Issue#44 Adding floating power to ninja under water

--- Divyank/Development/Ninja.java
+++ Divyank/Development/Ninja.java
@@ -6,14 +6,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class Ninja extends Actor
 {
+    int y=0;
+    int g=1;
     /**
      * Act - do whatever the Ninja wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Ninja(){
+    
+        GreenfootImage image = getImage();  
+        image.scale(60, 40);
+        setImage(image);
+    }
     public void act() 
     {
         // Add your action code here.
+        setLocation(getX(),getY()+y);
+        if(Greenfoot.isKeyDown("up")==true)
+        {
+            y=-10;
+        }
+        y=y+g;
+        
     }    
 }
