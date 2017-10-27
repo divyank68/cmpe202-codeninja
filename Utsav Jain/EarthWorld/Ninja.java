9a9c93630cb40bcc79fc53c348541f9b0cf1e95c
Utsav Jain <utsavjain1408@gmail.com>
2017-10-26 22:35:15 -0700

    Adding gravity to the EarthWorld

--- Utsav Jain/EarthWorld/Ninja.java
+++ Utsav Jain/EarthWorld/Ninja.java
@@ -12,6 +12,8 @@ public class Ninja extends Actor
      * Act - do whatever the Ninja wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int dy=0;
+    int g =2;
      public Ninja(){
     }
     public Ninja(int x, int y){
@@ -30,25 +32,49 @@ public class Ninja extends Actor
             if(Greenfoot.isKeyDown("right")){
                 move(2);
             }
+            
         }
+        else{
+             if(Greenfoot.isKeyDown("left")){
+                move(2);
+            }
+            if(Greenfoot.isKeyDown("right")){
+                move(-2);
+            }
+            if (Greenfoot.isKeyDown("up"))
+            {
+                setLocation(getX(), getY() + 6); 
+            }
+            //potential for momento design pattern
+            
+        }
+        if (Greenfoot.isKeyDown("up"))
+            {
+                setLocation(getX(), getY() - 6); 
+            }
     }
     public boolean isBarrier(){
     Actor mb;
     Actor tw;
     Actor w;
-    mb = getOneObjectAtOffset(18,18, MovingBrick.class);
+//    mb = !getIntersectingObjects(MovingBrick.class).isEmpty();
     tw = getOneObjectAtOffset(18,18, TempWall.class);
     w = getOneObjectAtOffset(18,18, wall.class);
-        if(mb != null || tw !=null || w !=null){
+        if(!getIntersectingObjects(MovingBrick.class).isEmpty()|| tw !=null || w !=null){
             return false;    
         }
         else
             return true;
     }
-    
+    public void fall (){
+        setLocation(getX(), getY()+dy);
+        dy = dy+g;
+    }
     public void act() 
     {
         // Add your action code here.
         move();
+        fall();
+        
     }    
 }
