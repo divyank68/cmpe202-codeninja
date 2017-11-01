dde25f9f3b665bf2092d9eb57e1e240dcd749a34
Divyank <divyank68@yahoo.co.in>
2017-10-31 23:17:52 -0700

    Issue#48 Implement Motion of characters for waterworld

--- Divyank/Development/Starfish.java
+++ Divyank/Development/Starfish.java
@@ -12,8 +12,13 @@ public class Starfish extends Actor
      * Act - do whatever the Starfish wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int timer=50;
+    private int direction = 5; 
     public void act() 
     {
         // Add your action code here.
+        if ((direction < 0 && getY() == 0) || (direction > 0 && getY() == getWorld().getHeight()-1)) direction = -direction;  
+        // movement
+        setLocation(getX(), getY() + direction);
     }    
 }
