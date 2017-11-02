b6c1345007b2469a3b45358c8edbe4efe411ea15
Divyank <divyank68@yahoo.co.in>
2017-11-01 23:53:54 -0700

    Implementing sound in water world

---
+++ Divyank/Development/SoundWorld.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class SoundWorld extends World
+{
+    GreenfootSound myMusic= new GreenfootSound("Skip_-01-_HyperSpaceAdventure.mp3");
+    private Background1[] bg = new Background1[3];
+    
+    private final int LEFT_BOUNDARY = -300;
+    private final int RIGHT_BOUNDARY = 900;
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public SoundWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 600, 1, false);
+        int x = LEFT_BOUNDARY;
+        for(int i=0; i<bg.length; i++)
+        {
+            bg[i] = new Background1();
+            addObject( bg[i], x, getHeight()/2 );
+            x=x+ 600;
+        }
+        addObject( new Person(), getWidth()/2, 190);
+    }
+    
+    public void scroll(int direction)
+    {
+        for(int i=0; i< bg.length; i++)
+        {
+            bg[i].setLocation(bg[i].getX()+direction, bg[i].getY() );
+        }
+    }
+    public void act()
+    {
+        myMusic.play();
+        if(Greenfoot.isKeyDown("left")) 
+        {
+            scroll(5);
+        }
+        if(Greenfoot.isKeyDown("right"))
+        {
+            scroll(-5);
+        }
+    }
+
+}
