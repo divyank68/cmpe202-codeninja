564c342cb82b29cc137cb9e4c45667d54eebfb87
janhudesai227 <janhudesai@gmail.com>
2017-11-10 17:00:55 -0800

    issue # 50 - Implemented the end of the stage

--- Jay/dev/FireWorld-CodeNinja/MyWorld.java
+++ Jay/dev/FireWorld-CodeNinja/MyWorld.java
@@ -44,6 +44,7 @@ public class MyWorld extends World
         barrier br2 =  new  barrier();
         Bomb bomb1 =  new  Bomb();
         barrel barrel1 =  new  barrel();
+        end end =  new  end();
         addObject(b1, 430, 280);
         addObject(b2, 150, 280);
         addObject(b3, 300, 190);
@@ -65,5 +66,6 @@ public class MyWorld extends World
         addObject(br2, 290, 380);
         addObject(bomb1, 450, 80);
         addObject(barrel1, 130, 75);
+        addObject(end, 565, 69);
     }
 }
