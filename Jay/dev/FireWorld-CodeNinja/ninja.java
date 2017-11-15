af947285e6891a8710114d4637d1029c06d48662
janhudesai227 <janhudesai@gmail.com>
2017-11-15 12:45:38 -0800

    Implemented Interface for the Strategy pattern

--- Jay/dev/FireWorld-CodeNinja/ninja.java
+++ Jay/dev/FireWorld-CodeNinja/ninja.java
@@ -60,13 +60,13 @@ public class ninja extends Actor
      */
     private void fire()
     {
-        if (fireflag &&  ! shuflag) {
-            fireballs suri1 =  new  fireballs();
+        if (fireflag && ! shuflag) {
+            fireballs suri1 =  new fireballs();
             getWorld().addObject(suri1, getX(), getY());
             suri1.setRotation(getRotation());
         }
-        if (shuflag &&  ! fireflag) {
-            Shuriken suri1 =  new  Shuriken();
+        if (shuflag && ! fireflag) {
+            Shuriken suri1 =  new Shuriken();
             getWorld().addObject(suri1, getX(), getY());
             suri1.setRotation(getRotation());
         }
@@ -172,7 +172,7 @@ public class ninja extends Actor
     public void checkobstacles()
     {
         Actor bricks = getOneIntersectingObject(Bricks.class);
-        if (bricks != null &&  ! onGround()) {
+        if (bricks != null && ! onGround()) {
             move(-5);
         }
     }
